dcf06bf26e65b62baf7c06495f3aae8d99ab412c
Nahuel José <nahueljose@gmail.com>
2019-01-29 18:44:02 -0300

    Demo usando imagenes estáticas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-/**
- * Import your modules and use them!
- */
-import { externalModule } from './js/myModule';
-
-/**
- * Import your styles!
- */
+import './js/BigInteger';
+import './js/zxing-pdf417';
 import './styles/main.scss';
 
-externalModule.greet();
+const image = document.createElement('img')
+image.src = require('./images/dni_sample.jpg');
+
+image.addEventListener('load', _ => {
+  document.body.appendChild(image);
+
+  const canvas = document.createElement('canvas');
+  const canvas_context = canvas.getContext('2d');
+  let source, binarizer, bitmap, result;
+
+  canvas.width = image.naturalWidth;
+  canvas.height = image.naturalHeight;
+  canvas_context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+  try {
+    source    = new ZXing.BitmapLuminanceSource(canvas_context, image);
+    binarizer = new ZXing.Common.HybridBinarizer(source);
+    bitmap    = new ZXing.BinaryBitmap(binarizer);
+    result    = ZXing.PDF417.PDF417Reader.decode(bitmap, null, false)
+    if( result && result[0] ) {
+      let p = document.createElement('p');
+      p.innerHTML = result[0].Text.split('@').join('<br />');
+      document.body.appendChild(p)
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
--- src/js/myModule.js
+++
@@ -1,8 +0,0 @@
-/**
- * Simple JS module to export
- */
-export const externalModule = {
-  greet() {
-    console.log('Hello from externalModule!');
-  }
-}
