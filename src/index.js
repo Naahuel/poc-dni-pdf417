d10cd819aa5f40251246c83c9e954bfe290bb304
Nahuel José <nahuel.jose@globant.com>
2019-01-30 11:35:59 -0300

    Demo con webcam live

--- src/index.js
+++ src/index.js
@@ -2,31 +2,78 @@ import './js/BigInteger';
 import './js/zxing-pdf417';
 import './styles/main.scss';
 
+// Crossbrowser getUserMedia
+navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+// Variables
+let _canvasWidth = 0, _canvasHeight = 0;
+let source, binarizer, bitmap, result;
+
+// Crear canvas para el video
+const canvas = document.createElement('canvas'),
+      ctx = canvas.getContext('2d');
+
+// Párrafo de resultado
+let resultP = document.createElement('p');
+document.body.appendChild(resultP)
+
+// Agregar canvas al body
+document.body.appendChild(canvas);
+
+// Crear elemento video e imagen
+const video = document.createElement('video');
+      video.setAttribute('autoplay',true);
 const image = document.createElement('img')
-image.src = require('./images/dni_sample.jpg');
 
-image.addEventListener('load', _ => {
-  document.body.appendChild(image);
+const startWebcam = () => { 
+  navigator.getUserMedia({ video: true, audio: false }, function(stream) {
+    video.srcObject = stream;
+  }, function(e) {
+    console.error('Error de cámara', e);
+  });
+}
 
-  const canvas = document.createElement('canvas');
-  const canvas_context = canvas.getContext('2d');
-  let source, binarizer, bitmap, result;
+let loopFrame;
 
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-  canvas_context.drawImage(image, 0, 0, canvas.width, canvas.height);
+const loop = () => {
+  // Loop principal
+  loopFrame = requestAnimationFrame(loop);
+  ctx.globalAlpha = 1;
+  image.src = canvas.toDataURL();
+  ctx.drawImage(video, 0, 0, _canvasWidth, _canvasHeight);
 
-  try {
-    source    = new ZXing.BitmapLuminanceSource(canvas_context, image);
-    binarizer = new ZXing.Common.HybridBinarizer(source);
-    bitmap    = new ZXing.BinaryBitmap(binarizer);
-    result    = ZXing.PDF417.PDF417Reader.decode(bitmap, null, false)
-    if( result && result[0] ) {
-      let p = document.createElement('p');
-      p.innerHTML = result[0].Text.split('@').join('<br />');
-      document.body.appendChild(p)
+  // Analizar frame
+
+  if( image.naturalWidth && image.naturalHeight ) {
+    try {
+      source    = new ZXing.BitmapLuminanceSource(ctx, image);
+      binarizer = new ZXing.Common.HybridBinarizer(source);
+      bitmap    = new ZXing.BinaryBitmap(binarizer);
+      result    = ZXing.PDF417.PDF417Reader.decode(bitmap, null, false)
+      if( result && result[0] ) {
+        resultP.innerHTML = result[0].Text.split('@').join('<br />');
+      }
+     
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
   }
+}
+
+const startLoop = () => { 
+  loopFrame = loopFrame || requestAnimationFrame(loop);
+}
+
+// Iniciar loop
+video.addEventListener('loadedmetadata',function(){
+  _canvasWidth  = canvas.width = video.videoWidth;
+  _canvasHeight = canvas.height = video.videoHeight;
+  startLoop();
 });
+
+
+// Inicializar
+startWebcam();
